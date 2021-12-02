99ef63c059bac0a6d9946b6f8b9a722dd25af77c
imranidz <66912044+imranidz@users.noreply.github.com>
2021-12-02 10:34:33 -0500

    updates to look more like css

--- scripts.js
+++ scripts.js
@@ -1,2 +1,25 @@
+import {$wrap, $element, $scrollAnimation} from "../../scripts/helpers.js";
+
 const hero = document.querySelector('.hero');
 console.log(hero);
+decorate();
+
+export function getRootPath() {
+    const loc = getLanguage();
+    if (loc === LANG.EN) {
+      return '/blog';
+    }
+    return `/${loc}/blog`;
+  }
+  
+
+export default function decorate($main) {
+
+    var team = document.querySelector("body > main");
+    console.log(team);
+    var content = team.querySelector("main:nth-child(2) > div:first-child")
+
+    //let bodyDiv = $element("table", [ $element("thead", [ $element("tr", [ $element("th", "Name"), $element("th", "Favorite Color") ]) ]), $element("tbody", [ $element("tr", [ $element("td", "Batman"), $element("td", "Black") ]) ]) ])
+
+    content.append(bodyDiv)
+}
---
+++ scripts/queries.js
@@ -0,0 +1,74 @@
+import { getRootPath } from "../../scripts.js";
+
+class QueryIndex {
+    constructor( index ) {
+        this.index = index;
+    }
+    findRecord( fn ) {
+        return this.index.fullindex.data.find(fn);
+    }
+    whereFieldContains(prop, val) {
+        return this.findRecord((rec) => {
+            if(typeof rec[prop] === "string") {
+                return rec[prop].indexOf(val) >= 0;
+            } else {
+                return rec[prop] === val;
+            }
+        });
+    }
+    whereUrlMatchesPath(url) {
+        return this.findRecord((rec) => {
+            return url.indexOf( rec.path ) >= 0;
+        });
+    }
+}
+
+/**
+ * fetches blog article index.
+ * @returns {object} index with data and path lookup
+ */
+
+export async function fetchBlogArticleIndex() {
+    const resp = await fetch(`${getRootPath()}/query-index.json`);
+    const json = await resp.json();
+    const byPath = {};
+    json.data.forEach((post) => {
+        byPath[post.path.split('.')[0]] = post;
+    });
+    const index = { data: json.data, byPath };
+    return (index);
+}
+
+/**
+ * fetches blog article index.
+ * @returns {object} index with data and path lookup
+ */
+
+export async function fetchIndex( options ) {
+    const resp = await fetch(`/query-index.json`);
+    const json = await resp.json();
+
+    const index = json;
+    return (index);
+}
+
+let cachedJson = null;
+let cachedQueryIndex = null;
+/**
+ * Resolves the index data, or re-uses it if its already been queried on the page
+ * @param {*} options
+ * @returns {QueryIndex}
+ */
+export async function resolveIndex( options ) {
+    if(!options) {
+        options = {};
+    }
+    if(!("force" in options)) {
+        options.force = false;
+    }
+    if(!cachedJson) {
+        cachedJson = await fetchIndex();
+        cachedQueryIndex = new QueryIndex(cachedJson);
+    }
+    return cachedQueryIndex;
+}
